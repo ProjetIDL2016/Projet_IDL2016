6b7b8716607b9924b60d03ce89c23f4ab610f8a0
Yoann <yoann.boulier@gmail.com>
2016-03-10 20:48:38 +0100

    Ajout Edition

---
+++ Modeling/ModelingYoann/Application/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using donnee;
+using edition;
+
+namespace Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Edition e = null;
+            Previsions p = null;
+            DateTime[] debut = new DateTime[2], fin = new DateTime[2];
+            double latitude1, longitude1, latitude2, longitude2;
+            double[] vitesse = new double[2], direction = new double[2], u = new double[2], v = new double[2], pression = new double[2];
+            int choix = 1;
+
+
+            Console.WriteLine("Définissez la zone géographique de la nouvelle prévision : ");
+            Console.WriteLine("Coordonnées du premier point :");
+            Console.WriteLine("Latitude :");
+            latitude1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Longitude :");
+            longitude1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Coordonnées du deuxième point :");
+            Console.WriteLine("Latitude :");
+            latitude2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Longitude :");
+            longitude2 = Convert.ToDouble(Console.ReadLine());
+
+            while (choix == 1) {
+                Console.WriteLine("Entrez la date et l'heure de début de la prévision (dd/mm/yyyy hh:mm:ss) :");
+                debut[0] = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Entrez la date et l'heure de début de la prévision (dd/mm/yyyy hh:mm:ss) :");
+                debut[0] = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Entrez la vitesse du vent en kmH");
+                vitesse[0] = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Entrez la direction du vent en degré (entre 0 et 360) :");
+                direction[0] = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Entrez la pression atmosphérique en hPa :");
+                pression[0] = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("\nQue voulez vous faire ?");
+                Console.WriteLine("1: Ajouter une prévision");
+                Console.WriteLine("2: Valider");
+                Console.WriteLine("3: Annuler");
+                choix = Convert.ToInt32(Console.ReadLine());
+            }
+
+            e = new Edition();
+            p = e.creerPrevision(latitude1, longitude1, latitude2, longitude2, debut, fin, vitesse, direction, pression, TypeVitesseVent.KilometreHeure);
+
+            Console.WriteLine(p.ToString());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Modeling/ModelingYoann/ModelingYoannLib/GeneratedCode/Tests/TestEdition.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using donnee;
+using conversion;
+using edition;
+
+namespace TestUnitaires
+{
+    [TestClass]
+    public class UnitTestEdition
+    {
+        Edition e = null;
+        Previsions p = null;
+        DateTime[] debut = new DateTime[2], fin = new DateTime[2];
+        double[] vitesse = new double[2], direction = new double[2], u = new double[2], v = new double[2], pression = new double[2];
+        String ResultatObtenu, ResultatAttendu;
+
+        [TestMethod]
+        public void TestEditionPrevisionExiste()
+        {
+            debut[0] = new DateTime(2016, 3, 10, 11, 0, 0);
+            debut[1] = new DateTime(2016, 3, 10, 12, 0, 0);
+            fin[0] = new DateTime(2016, 3, 10, 12, 0, 0);
+            fin[1] = new DateTime(2016, 3, 10, 13, 0, 0);
+
+            vitesse[0] = 50;
+            vitesse[1] = 110;
+            direction[0] = 180;
+            direction[1] = 90;
+
+            u[0] = conversion.Conversion.obtenirU(direction[0], vitesse[0]);
+            u[1] = conversion.Conversion.obtenirU(direction[1], vitesse[1]);
+            v[0] = conversion.Conversion.obtenirV(direction[0], vitesse[0]);
+            v[1] = conversion.Conversion.obtenirV(direction[1], vitesse[1]);
+
+            pression[0] = 1015;
+            pression[1] = 1015;
+
+
+            e = new Edition();
+            p = e.creerPrevision(47, 58, 48, 57, debut, fin, u, v, pression, TypeVitesseVent.KilometreHeure);
+
+            ResultatObtenu = p.getVent(2016, 3, 10, 11, 47, 58).ToString();
+            ResultatAttendu = "10/03/2016 11h : lat: 47 long: 57 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 11h : lat: 47 long: 57,5 p: 1015 dir: 180 v: 50\n" 
+                            + "10/03/2016 11h : lat: 47 long: 58 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 11h : lat: 47,5 long: 57 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 11h : lat: 47,5 long: 57,5 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 11h : lat: 47,5 long: 58 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 11h : lat: 48 long: 57 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 11h : lat: 48 long: 57,5 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 11h : lat: 48 long: 58 p: 1015 dir: 180 v: 50\n"
+                            + "10/03/2016 12h : lat: 47 long: 57 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 47 long: 57,5 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 47 long: 58 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 47,5 long: 57 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 47,5 long: 57,5 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 47,5 long: 58 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 48 long: 57 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 48 long: 57,5 p: 1015 dir: 90 v: 110\n"
+                            + "10/03/2016 12h : lat: 48 long: 58 p: 1015 dir: 90 v: 110";
+
+            Assert.IsNotNull(p);
+            Assert.IsFalse(ResultatObtenu.Equals(ResultatAttendu));
+        }
+    }
+}
