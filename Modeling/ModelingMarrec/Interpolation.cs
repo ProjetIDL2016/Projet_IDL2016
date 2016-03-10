7cab61acae1857cf91d8bf32bcd08ceedee1ab58
Cédric MARREC <cedric.marrec29@gmail.com>
2016-03-10 16:51:37 +0100

    obtenirU obtenirV

---
+++ Modeling/ModelingMarrec/Interpolation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MathHelper;
+
+public class Interpolation
+{
+    float resInterpolation = 0;
+    public static double interpolation(double u, double v)
+    {
+        resInterpolation = MathHelper.Lerp();
+        return resInterpolation;
+    }
+}
---
+++ Modeling/ModelingMarrec/Test2/Conversion/TestConversion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Conversion
+{
+    class TestConversion
+    {
+        static void main(string[] arg)
+        {
+            Console.ReadLine();
+        }
+
+    }
+}
+
